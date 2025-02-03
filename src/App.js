40369c8b6619abab98b5d4366bed14a1c8dc5952
Jaykishor Chauhan <jaykishor04.3.7.np@gmail.com>
2025-02-04 00:24:00 +0530

    Conainer class utility commii

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
-import Button from './Button';
-import Tutorial03 from './Tutorial03';
+import Container from './Container';
+// import Button from './Button';
+// import Tutorial03 from './Tutorial03';
 // import Tutorial01 from './Tutorial01';
 
 function App() {
   return (
     <div className="">
       {/* <Tutorial01 /> */}
-      <Button/>
-      <Tutorial03/>
+      {/* <Button/> */}
+      {/* <Tutorial03/> */}
+      <Container />
     </div>
   );
 }
---
+++ src/Container.js
@@ -0,0 +1,38 @@
+import React from 'react'
+
+function Container() {
+  return (
+    <div>
+      <div className="container my-5">
+        <h1>Normal or defult Contaner</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam ab explicabo, fugiat atque quod quia, quo ipsum pariatur libero dolores obcaecati corrupti molestiae doloremque expedita dignissimos accusantium dicta aliquid veniam.</p>
+      </div>
+      <div className="container-sm my-5">
+        <h1>Small Contaner</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam ab explicabo, fugiat atque quod quia, quo ipsum pariatur libero dolores obcaecati corrupti molestiae doloremque expedita dignissimos accusantium dicta aliquid veniam.</p>
+      </div>
+      <div className="container-md my-5">
+        <h1>Median Contaner</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam ab explicabo, fugiat atque quod quia, quo ipsum pariatur libero dolores obcaecati corrupti molestiae doloremque expedita dignissimos accusantium dicta aliquid veniam.</p>
+      </div>
+      <div className="container-lg my-5">
+        <h1>Large Contaner</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam ab explicabo, fugiat atque quod quia, quo ipsum pariatur libero dolores obcaecati corrupti molestiae doloremque expedita dignissimos accusantium dicta aliquid veniam.</p>
+      </div>
+      <div className="container-xl my-5">
+        <h1>xl Contaner</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam ab explicabo, fugiat atque quod quia, quo ipsum pariatur libero dolores obcaecati corrupti molestiae doloremque expedita dignissimos accusantium dicta aliquid veniam.</p>
+      </div>
+      <div className="container-xxl my-5">
+        <h1>xxl Contaner</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam ab explicabo, fugiat atque quod quia, quo ipsum pariatur libero dolores obcaecati corrupti molestiae doloremque expedita dignissimos accusantium dicta aliquid veniam.</p>
+      </div>
+      <div className="container-fluid my-5">
+        <h1>Fluid Contaner</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam ab explicabo, fugiat atque quod quia, quo ipsum pariatur libero dolores obcaecati corrupti molestiae doloremque expedita dignissimos accusantium dicta aliquid veniam.</p>
+      </div>
+    </div>
+  )
+}
+
+export default Container
